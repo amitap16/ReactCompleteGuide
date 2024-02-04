bf9298ef12881eb6bbedde3e81fcf5741b0bdff5
Amita Patel <amitap16@live.com>
2024-02-04 11:06:54 -0500

    111. Scoping CSSRules with CSS Modules

--- 07_Styling/src/components/Header.jsx
+++ 07_Styling/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css'
+import clssses from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+          <h1>ReactArt</h1>
+          <p className={clssses.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
