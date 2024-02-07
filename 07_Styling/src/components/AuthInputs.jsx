69f05f4b3589d80d39e8ff920afd9b91e6a02438
Amita Patel <amitap16@live.com>
2024-02-06 21:06:53 -0500

    119-121-Tailwind: Media Queries & Pseudo Selectors; Dynamic & Conditional Styling with Tailwind; Migrating the Demo App to Tailwind CSS

--- 07_Styling/src/components/AuthInputs.jsx
+++ 07_Styling/src/components/AuthInputs.jsx
@@ -1,20 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControllContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const TextButton = styled.button`
-  color: #f0b322;
-  border: none;
-
-  &:hover { color: #f0920e; }`;
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('');
@@ -37,8 +23,9 @@ export default function AuthInputs() {
     const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
     return (
-        <div id="auth-inputs">
-            <ControllContainer>
+        <div id="auth-inputs"
+            className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+            <div className="flex flex-col gap-2 mb-6">
                 <Input
                     label="Email"
                     invalid={emailNotValid}
@@ -53,12 +40,12 @@ export default function AuthInputs() {
                         handleInputChange('password', event.target.value)
                     }
                 />
-            </ControllContainer>
-            <div className="actions">
-                <TextButton type="button" className="text-button">
+            </div>
+            <div className="flex justify-end gap-4">
+                <button type="button" className="text-amber-400 hover:test-amber-500">
                     Create a new account
-                </TextButton>
-                <Button className='button' onClick={handleLogin}>Sign In</Button>
+                </button>
+                <Button onClick={handleLogin}>Sign In</Button>
             </div>
         </div>
     );
